96d64740731c42a47e9e5a46d0b135641188f7c7
André Valle <andrevvalle@icloud.com>
2016-05-20 14:54:35 -0300

    Change zshrc and remove softwares

--- softwares/install.sh
+++ softwares/install.sh
@@ -6,13 +6,10 @@ brew tap phinze/homebrew-cask
 brew install brew-cask
 
 # browser
-brew cask install firefox-aurora
 brew cask install google-chrome
-brew cask install google-chrome-canary
-brew cask install opera-next
 
 # development
-brew cask install sublime-text-3
+brew cask install sublime-text
 brew cask install iterm2
 brew cask install virtualbox
 
@@ -21,7 +18,6 @@ brew cask install alfred
 brew cask install spectacle
 brew cask install dropbox
 brew cask install mou
-brew cask install skype
+brew cask install skyp
 brew cask install spotify
 brew cask install slack
-brew cask install u-torrent
