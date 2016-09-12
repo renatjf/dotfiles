7927adb593c8cc53e3837789bab3faa864a0b437
Renato Freitas <renatjf@gmail.com>
2016-09-12 12:37:29 -0300

    Update install.sh

--- softwares/install.sh
+++ softwares/install.sh
@@ -5,23 +5,12 @@ msg_install “Installing Brew Cask”
 brew tap phinze/homebrew-cask
 brew install brew-cask
 
-# browser
-brew cask install google-chrome
-
 # development
-brew cask install sublime
 brew cask install iterm2
 brew cask install virtualbox
 
 # other
 brew cask install alfred
 brew cask install spectacle
-brew cask install dropbox
-brew cask install mou
-brew cask install skype
-brew cask install spotify
-brew cask install slack
 brew cask install franz
-brew cask install appcleaner
 brew cask install caffeine
-brew cask install nylas-n1
