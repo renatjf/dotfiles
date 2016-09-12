f947aee3168a29c22d041df43c3e2e1b9ae97a61
Renato Freitas <renatjf@gmail.com>
2016-09-12 12:34:35 -0300

    Update global.sh

--- ruby/global.sh
+++ ruby/global.sh
@@ -6,21 +6,3 @@ if which gem &> /dev/null; then
     sudo gem update --system
     echo "gem"
 fi
-
-# capistrano
-if which cap &> /dev/null; then
-    msg_checking "capistrano"
-else
-    msg_install "capistrano" "gem install capistrano"
-    sudo gem install capistrano
-    msg_ok "capistrano"
-fi
-
-# jekyll
-if which jekyll &> /dev/null; then
-    msg_checking "jekyll"
-else
-    msg_install "jekyll" "gem install jekyll"
-    sudo gem install jekyll
-    msg_ok "jekyll"
-fi
